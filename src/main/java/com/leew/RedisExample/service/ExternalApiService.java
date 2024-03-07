7fe1073ca86b3efa36c289654258efa34a68f12a
yw824 <yongwoo25@gmail.com>
2024-03-07 21:06:35 +0900

    <feat> : Spring Cache 라이브러리를 이용한 캐싱 - application.yml 파일에 스프링 라이브러리로 생성되는 캐시의 저장소를 redis로 설정 - userAge 변수를 Annotation만 부여하는 것으로도 캐싱 설정 가능 	- Application에도, 캐시를 사용할 수 있음을 알리는 Annotation 부여 - userName은 RedisTemplate로 직접 설정한 것과 달리, userEmail은 스프링 캐싱 라이브러리로 설정

--- src/main/java/com/leew/RedisExample/service/ExternalApiService.java
+++ src/main/java/com/leew/RedisExample/service/ExternalApiService.java
@@ -1,11 +1,13 @@
 package com.leew.RedisExample.service;
 
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.cache.annotation.Cacheable;
 import org.springframework.stereotype.Service;
 
 @Service
 @Slf4j
 public class ExternalApiService {
+    // RedisTemplate 객체를 사용해 직접 캐싱
     public String getUserName(String userId) {
         // 외부 서비스나 DB 호출
         // 좀 시간이 걸린다고 가정
@@ -24,6 +26,8 @@ public class ExternalApiService {
         return "Error: None";
     }
 
+    // 스프링의 캐싱 라이브러리를 이용해 yml에 설정한 대로 캐시를 redis에 저장
+    @Cacheable(cacheNames = "userAgeCache", key="#userId") // key를 명시적으로 지정
     public int getUserAge(String userId) {
         // 외부 서비스나 DB 호출
         // 좀 시간이 걸린다고 가정
