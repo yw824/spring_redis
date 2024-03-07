e9ab4ff189bd096b6cd831636bf94c35ea886739
yw824 <yongwoo25@gmail.com>
2024-03-07 20:34:48 +0900

    <feat> : Redis 캐시 사용하여 시간 줄이기 - userName에는 Redis 캐시 사용하여 값 가져오기 	- 초기 접근에서는 값이 없다. 그리고 써야 한다. 		- timeout도 설정 가능(생명 주기) - userAge는 Redis 사용 안하고 그대로 불러오기 및 sleep 수행 - docker로 Redis를 실행한 후, 서버로 A, B를 요청하여 Redis에 쓴 후에, 	- redis-cli로 getKey 하면, 실제로 키가 쓰여 있는 것이 보인다.

--- src/main/java/com/leew/RedisExample/service/UserService.java
+++ src/main/java/com/leew/RedisExample/service/UserService.java
@@ -2,7 +2,11 @@ package com.leew.RedisExample.service;
 
 import com.leew.RedisExample.dto.UserProfile;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.redis.core.StringRedisTemplate;
+import org.springframework.data.redis.core.ValueOperations;
 import org.springframework.stereotype.Service;
+
+import java.util.concurrent.TimeUnit;
 
 @Service
 public class UserService {
@@ -10,8 +14,26 @@ public class UserService {
     @Autowired
     private ExternalApiService externalApiService;
 
+    @Autowired
+    StringRedisTemplate redisTemplate;
+
     public UserProfile getUserProfile(String userId) {
-        String userName = externalApiService.getUserName(userId);
+
+        String userName = null;
+
+        // 요청을 하기 전에, Redis를 먼저 구현
+        ValueOperations<String, String> ops = redisTemplate.opsForValue();
+        String cachedName = ops.get("nameKey: " + userId);
+        if(cachedName != null) {
+
+        } else {
+            userName = externalApiService.getUserName(userId);
+            // 가져온 후에 캐시에 담는 과정이 필요함
+            // Cache-Aside 과정 - 읽기 과정에서 캐시 탐색
+            ops.set("nameKey: " + userId, userName, 5, TimeUnit.SECONDS);
+        }
+
+        // userAge는 그대로 구해오는 과정이기 때문에, 시간 timeout이 그대로 걸린다.
         int userAge = externalApiService.getUserAge(userId);
 
         return new UserProfile(userName, userAge);
